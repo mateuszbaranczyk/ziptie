03f8e8cda4c90daeccecb8dd9bda5effd96b21e1
kundzior <grzybek2413@gmail.com>
2024-09-02 18:06:42 +0200

    crud validation

--- app/crud.py
+++ app/crud.py
@@ -1,6 +1,9 @@
 from sqlalchemy.orm import Session
 
 from app import models, schemas
+from sqlalchemy.exc import IntegrityError
+
+from fastapi import HTTPException
 
 
 def create_author(author: schemas.AuthorCreate, db: Session) -> models.Author:
@@ -16,13 +19,19 @@ def create_book(book: schemas.Book, db: Session) -> models.Book:
 
 
 def get_author(author_id: int, db: Session) -> models.Author:
-    return (
+    author = (
         db.query(models.Author).filter(models.Author.id == author_id).first()
     )
+    if not author:
+        raise HTTPException(status_code=404, detail="Not found")
+    return author
 
 
 def db_add(item: models.Author | models.Book, db: Session) -> None:
-    db.add(item)
-    db.commit()
-    db.refresh(item)
+    try:
+        db.add(item)
+        db.commit()
+        db.refresh(item)
+    except IntegrityError:
+        raise HTTPException(status_code=400, detail="Already created.")
     return None
