923c241df3a65b0ad4d22c0db1a07295f02f343d
kundziorro <mateusz.baranczyk@gmail.com>
2024-09-02 23:30:16 +0200

    sql env

--- app/database.py
+++ app/database.py
@@ -1,8 +1,16 @@
+import os 
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+user = os.getenv("MYSQL_USER", "root")
+password = os.getenv("MYSQL_ROOT_PASSWORD", "password")
+host = os.getenv("MYSQL_HOST", "api-database")
+port = os.getenv("MYSQL_PORT", "3306")
+database = os.getenv("MYSQL_DATABASE", "api")
+
+SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{user}:{password}@{host}:{port}/{database}"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
