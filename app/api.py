d2358b821fa5dedce11ffc4649cf53a96c911ae7
kundzior <grzybek2413@gmail.com>
2024-09-02 16:47:07 +0200

    crud usage fix

--- app/api.py
+++ app/api.py
@@ -4,7 +4,7 @@ from app.schemas import AuthorCreate, Author, Book, BookCreate
 from app.database import engine, get_db
 from app import models
 from sqlalchemy.orm import Session
-from app.crud import create_author, create_book, get_author
+from app import crud
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -18,17 +18,17 @@ async def smoke():
 
 @app.post("/author", response_model=Author)
 async def create_author(author: AuthorCreate, db: Session = Depends(get_db)):
-    author_db = create_author(author, db)
+    author_db = crud.create_author(author, db)
     return author_db
 
 
 @app.post("/book", response_model=Book)
 async def create_book(book: BookCreate, db: Session = Depends(get_db)):
-    book_db = create_book(book)
+    book_db = crud.create_book(book, db)
     return book_db
 
 
 @app.get("/author-books/{author_id}", response_model=Author)
-async def get_author_books(author_id: int):
-    author_db = get_author(author_id)
+async def get_author_books(author_id: int, db: Session = Depends(get_db)):
+    author_db = crud.get_author(author_id, db)
     return author_db
