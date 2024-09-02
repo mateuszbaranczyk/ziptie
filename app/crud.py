dd68579234760c404ba5726c4300d845a03e5c2c
kundzior <grzybek2413@gmail.com>
2024-09-02 15:59:44 +0200

    using db  fix

--- app/crud.py
+++ app/crud.py
@@ -3,25 +3,25 @@ from sqlalchemy.orm import Session
 from app import models, schemas
 
 
-def create_author(db: Session, author: schemas.AuthorCreate) -> models.Author:
+def create_author(author: schemas.AuthorCreate, db: Session) -> models.Author:
     author_db = models.Author(**author.model_dump())
-    db_add(author_db)
+    db_add(author_db, db)
     return author_db
 
 
-def create_book(db: Session, book: schemas.Book) -> models.Book:
+def create_book(book: schemas.Book, db: Session) -> models.Book:
     book_db = models.Book(**book.model_dump())
-    db_add(book_db)
+    db_add(book_db, db)
     return book_db
 
 
-def get_author(db: Session, author_id: int) -> models.Author:
+def get_author(author_id: int, db: Session) -> models.Author:
     return (
         db.query(models.Author).filter(models.Author.id == author_id).first()
     )
 
 
-def db_add(item: models.Author | models.Book, db: Session = Session) -> None:
+def db_add(item: models.Author | models.Book, db: Session) -> None:
     db.add(item)
     db.commit()
     db.refresh(item)
